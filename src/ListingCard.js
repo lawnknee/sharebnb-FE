decd44be420dcef644f1a88ef03ea061f00c2bb1
Loni <kuangloni@gmail.com>
2021-08-25 12:33:18 -0700

    ListingCard component and styling

---
+++ src/ListingCard.js
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+/** Renders a single Listing Component
+ *  Contains information about the listing
+ *
+ * Props:
+ *  - listing: an objec like : { id, title, city, state, country, host_id, photoPath, price, details, host }
+ *      where host is [{ id, firstName, lastName }]
+ *
+ * ListingList -> ListingCard
+ */
+
+function ListingCard({ listing }) {
+  console.log(listing);
+
+  const {
+    id,
+    title,
+    city,
+    state,
+    photoUrl,
+  } = listing;
+
+  return (
+    <div className="ListingCard container mb-4">
+      <Link
+        to={`/listing/${id}`}
+        style={{ textDecoration: "none" }}
+      >
+        <div className="ListingCard-card card mt-5">
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <small className="card-text">{city}, {state}</small>
+          </div>
+          {photoUrl && (
+            <img src={photoUrl} alt={title}></img>
+          )}
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default ListingCard;
