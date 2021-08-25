b1829b3a07246820e7d1382a75df70ac2bef8283
Loni <kuangloni@gmail.com>
2021-08-25 12:32:15 -0700

    Loading component and styling

---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loading.css";
+
+/** Renders a Loading message during API requests. */
+
+export default function Loading() {
+  return (
+    <div className="Loading">
+      <div className="Loading-msg">Loading...</div>
+    </div>
+  )
+}
