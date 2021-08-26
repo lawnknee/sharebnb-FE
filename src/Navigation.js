17e193eb20f057ecb8d1ee7d5100072a3ef08046
Loni <kuangloni@gmail.com>
2021-08-26 14:21:32 -0700

    added private routes

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import "./Navigation.css";
-import React from "react";
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
 
 /** Navigation bar for site. Shows up on every page.
  *
@@ -10,26 +11,65 @@ import React from "react";
  * Rendered by App.
  */
 
-function Navigation() {
-  return (
-    <nav className="Navbar navbar navbar-dark bg-primary d-flex bd-highlight mb-4">
-      <div className="me-auto pl-0 bd-highlight navbar-brand">
-        <NavLink exact to="/">
-          Sharebnb
-        </NavLink>
-      </div>
-      <div className="me-0 bd-highlight">
-        <NavLink exact to="/listings">
-          Listings
-        </NavLink>
-      </div>
-      <div className="me-0 bd-highlight">
-        <NavLink exact to="/listings/create">
-          Become a host
-        </NavLink>
-      </div>
-    </nav>
-  );
+function Navigation({ logout }) {
+  const { currentUser } = useContext(UserContext);
+  console.debug("Navigation", "currentUser=", currentUser);
+
+  function loggedInNav() {
+    return (
+      <nav className="Navbar navbar navbar-dark bg-primary d-flex bd-highlight mb-4">
+        <div className="me-auto pl-0 bd-highlight navbar-brand">
+          <NavLink exact to="/">
+            Sharebnb
+          </NavLink>
+        </div>
+        <div className="me-0 bd-highlight">
+          <NavLink exact to="/listings">
+            Listings
+          </NavLink>
+        </div>
+        <div className="me-0 bd-highlight">
+          <NavLink exact to="/listings/create">
+            Become a host
+          </NavLink>
+        </div>
+        <div className="me-0 bd-highlight">
+          <NavLink exact to="/" onClick={logout}>
+            Log out
+          </NavLink>
+        </div>
+      </nav>
+    );
+  }
+
+  function loggedOutNav() {
+    return (
+      <nav className="Navbar navbar navbar-dark bg-primary d-flex bd-highlight mb-4">
+        <div className="me-auto pl-0 bd-highlight navbar-brand">
+          <NavLink exact to="/">
+            Sharebnb
+          </NavLink>
+        </div>
+        <div className="me-0 bd-highlight">
+          <NavLink exact to="/listings">
+            Listings
+          </NavLink>
+        </div>
+        <div className="me-0 bd-highlight">
+          <NavLink exact to="/login">
+            Login
+          </NavLink>
+        </div>
+        <div className="me-0 bd-highlight">
+          <NavLink exact to="/signup">
+            Sign Up
+          </NavLink>
+        </div>
+      </nav>
+    );
+  }
+
+  return <div>{currentUser ? loggedInNav() : loggedOutNav()}</div>;
 }
 
 export default Navigation;
--- src/Routes.js
+++ src/Routes.js
@@ -4,28 +4,58 @@ import Homepage from "./Homepage.js";
 import NotFound from "./Notfound";
 import Listings from "./ListingsContainer";
 import CreateListingForm from "./CreateListingForm"
+import LoginForm from "./LoginForm.js";
+import SignupForm from "./SignupForm.js";
+import ListingDetails from "./ListingDetails.js";
+import PrivateRoute from "./PrivateRoute";
 
 /** Routes for Sharebnb App
  * 
+ * Parts of site should only be visitable when logged in. Those routes are
+ * wrapped by <PrivateRoute>, which is an authorization component.
+ *  
  *  Context:
  *      - UserContext
  *
  *  App -> Routes
  */
 
-function Routes({ create }) {
+function Routes({ create, login, register}) {
+  console.debug(
+    "Routes",
+    `login=${typeof login}`,
+    `register=${typeof register}`,
+  );
+
   return(
     <Switch>
+
       <Route exact path="/">
         <Homepage />
       </Route>
+
+      <Route exact path="/login">
+        <LoginForm login={login} />
+      </Route>
+
+      <Route exact path="/signup">
+        <SignupForm register={register} />
+      </Route>
+
       <Route exact path="/listings">
         <Listings />
       </Route>
-      <Route exact path="/listings/create">
-        <CreateListingForm create={create}/>
+
+      <Route exact path="/listing/:id">
+        <ListingDetails />
       </Route>
+
+      <PrivateRoute exact path="/listings/create">
+        <CreateListingForm create={create}/>
+      </PrivateRoute>
+
      <Route><NotFound /></Route>
+     
     </Switch>
   );
 }
