eebad7db4f1e969bc2de00816c8dd71096b040ef
Loni <kuangloni@gmail.com>
2021-09-29 17:06:51 -0700

    removed uneccessary imports and changed Nav css

--- src/Messages.js
+++ src/Messages.js
@@ -1,7 +1,6 @@
 import { useState, useEffect, useContext } from "react";
 import SharebnbApi from "./api";
 import UserContext from "./UserContext";
-import "./Messages.css"
 
 /**  Messages Component
  *
