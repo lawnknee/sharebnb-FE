b560dfe3d3644881dfa5cf885ed48dfc4287a0b9
Loni <kuangloni@gmail.com>
2021-10-17 10:33:32 -0700

    listing details page can be seen by non-logged in. message modal only shows for logged in

--- src/ListingDetails.js
+++ src/ListingDetails.js
@@ -55,7 +55,6 @@ function ListingDetails({ sendMessage }) {
     } catch (err) {
       setAlert(err);
     }
-    // document.getElementById("messageModal").modal('hide');
     setAlert([...alert, "Message sent!"]);
   }
 
@@ -104,86 +103,90 @@ function ListingDetails({ sendMessage }) {
               >
                 Contact host
               </button>
+            
+            
             ) : (
               <Link to="/login" className="btn btn-primary">
                 Sign in to contact host
               </Link>
             )}
 
-            <div
-              className="modal fade"
-              id="messageModal"
-              tabIndex="-1"
-              aria-labelledby="Contact Host"
-              aria-hidden="true"
-            >
-              <div className="modal-dialog">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title" id="messageModal">
-                      New message to {listing.host.firstName}
-                    </h5>
-                    <button
-                      type="button"
-                      className="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div className="modal-body">
-                    <form onSubmit={handleSubmit}>
-                      <div className="mb-3">
-                        <label htmlFor="sender-name" className="col-form-label">
-                          From:
-                        </label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          id="sender-name"
-                          value={currentUser.username}
-                          onChange={handleChange}
-                          required
-                          readOnly
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <label
-                          htmlFor="message-text"
-                          className="col-form-label"
-                        >
-                          Message:
-                        </label>
-                        <textarea
-                          name="text"
-                          className="form-control"
-                          id="message-text"
-                          onChange={handleChange}
-                          value={formData.text}
-                          rows="5"
-                          required
-                        />
-                      </div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-secondary"
-                          data-bs-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                        <button
-                          type="submit"
-                          data-bs-dismiss="modal"
-                          className="btn btn-primary"
-                        >
-                          Send message
-                        </button>
-                      </div>
-                    </form>
+            {currentUser && (
+              <div
+                className="modal fade"
+                id="messageModal"
+                tabIndex="-1"
+                aria-labelledby="Contact Host"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="messageModal">
+                        New message to {listing.host.firstName}
+                      </h5>
+                      <button
+                        type="button"
+                        className="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                      ></button>
+                    </div>
+                    <div className="modal-body">
+                      <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                          <label htmlFor="sender-name" className="col-form-label">
+                            From:
+                          </label>
+                          <input
+                            type="text"
+                            className="form-control"
+                            id="sender-name"
+                            value={currentUser.username}
+                            onChange={handleChange}
+                            required
+                            readOnly
+                          />
+                        </div>
+                        <div className="mb-3">
+                          <label
+                            htmlFor="message-text"
+                            className="col-form-label"
+                          >
+                            Message:
+                          </label>
+                          <textarea
+                            name="text"
+                            className="form-control"
+                            id="message-text"
+                            onChange={handleChange}
+                            value={formData.text}
+                            rows="5"
+                            required
+                          />
+                        </div>
+                        <div className="modal-footer">
+                          <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                          >
+                            Close
+                          </button>
+                          <button
+                            type="submit"
+                            data-bs-dismiss="modal"
+                            className="btn btn-primary"
+                          >
+                            Send message
+                          </button>
+                        </div>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       </div>
